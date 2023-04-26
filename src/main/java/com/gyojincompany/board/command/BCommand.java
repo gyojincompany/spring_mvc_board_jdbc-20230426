7f617435d2787efea803eff3141029146e7914a5
ICI <ICI@DESKTOP-EA5M9P6>
2023-04-26 11:59:35 +0900

    write, list, content_view jdbc로 변환 완료

---
+++ src/main/java/com/gyojincompany/board/command/BCommand.java
@@ -0,0 +1,11 @@
+package com.gyojincompany.board.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+public interface BCommand {
+	
+	public void execute(Model model);
+
+}
---
+++ src/main/java/com/gyojincompany/board/dao/Constant/Constant.java
@@ -0,0 +1,7 @@
+package com.gyojincompany.board.dao.Constant;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Constant {
+	public static JdbcTemplate template;//전역 객체
+}
